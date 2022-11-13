ebe9f81af11230c4849bee2e9f457c25aca6d9ed
Kitatzu <exe922@gmail.com>
2022-11-13 00:09:58 -0300

    Update Backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,16 +2,16 @@ const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const videogameRouter = require ('./videogames');
-const genreRouter = require ('./genre');
+const videogamesRouter = require ('./videogames');
+const genresRouter = require ('./genres');
 const videogameRouter = require ('./videogame')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-router.use ('/videogames', videogameRouter);
-router.use ('/genre', genreRouter);
+router.use ('/videogames', videogamesRouter);
+router.use ('/genres', genresRouter);
 router.use ('/videogame', videogameRouter)
 
 module.exports = router;
